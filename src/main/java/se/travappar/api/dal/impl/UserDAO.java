8fdc3a4c1f3bb87c5f66e50f45acc8dd2b699a7d
zayats <sanyazayats@gmail.com>
2015-12-04 00:35:44 +0100

    change getting user by device_id

--- src/main/java/se/travappar/api/dal/impl/UserDAO.java
+++ src/main/java/se/travappar/api/dal/impl/UserDAO.java
@@ -3,9 +3,19 @@ package se.travappar.api.dal.impl;
 import se.travappar.api.dal.AbstractDAO;
 import se.travappar.api.model.Users;
 
+import java.util.List;
+
 public class UserDAO extends AbstractDAO<Users> {
     public UserDAO() {
         super(Users.class);
         id_column  = "device_id";
+    }
+
+    public Users get(String id) {
+        List<Users> list = (List<Users>) getHibernateTemplate().find("from " + Users.class.getSimpleName() + " where " + id_column + "=?", id);
+        if (list.size() > 0) {
+            return list.get(0);
+        }
+        return null;
     }
 }
