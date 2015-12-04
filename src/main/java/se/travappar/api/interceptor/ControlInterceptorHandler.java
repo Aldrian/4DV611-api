6cc28c35c0ea6acbc0e7ec39ee0eb273867cc101
zayats <sanyazayats@gmail.com>
2015-12-04 12:36:36 +0100

    we need moooore logs

--- src/main/java/se/travappar/api/interceptor/ControlInterceptorHandler.java
+++ src/main/java/se/travappar/api/interceptor/ControlInterceptorHandler.java
@@ -1,5 +1,7 @@
 package se.travappar.api.interceptor;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -8,8 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ControlInterceptorHandler extends HandlerInterceptorAdapter {
 
+    private static final Logger logger = LogManager.getLogger(ControlInterceptorHandler.class);
+
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        logger.info("Handling request " + httpServletRequest.getMethod() + " with headers " + httpServletRequest.getHeaderNames());
         httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
         return true;
     }
@@ -18,6 +23,7 @@ public class ControlInterceptorHandler extends HandlerInterceptorAdapter {
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
         httpServletResponse.addHeader("Access-Control-Request-Methods", "OPTIONS, PUT, GET, DELETE, POST");
+        logger.info("Request " + httpServletRequest.getMethod() + " handled with resonse headers " + httpServletResponse.getHeaderNames());
     }
 
 }
