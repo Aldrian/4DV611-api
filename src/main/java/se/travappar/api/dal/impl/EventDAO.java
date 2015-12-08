c9b33c1936538f3f1fb5445c1efd7b6396286202
zayats <sanyazayats@gmail.com>
2015-12-08 02:12:13 +0100

    add sorting Events by date

--- src/main/java/se/travappar/api/dal/impl/EventDAO.java
+++ src/main/java/se/travappar/api/dal/impl/EventDAO.java
@@ -1,11 +1,22 @@
 package se.travappar.api.dal.impl;
 
+import org.springframework.transaction.annotation.Transactional;
 import se.travappar.api.dal.AbstractDAO;
 import se.travappar.api.model.Event;
+
+import java.util.List;
 
 public class EventDAO extends AbstractDAO<Event> {
 
     public EventDAO() {
         super(Event.class);
     }
+
+    @Transactional
+    @Override
+    public List<Event> getList() {
+        List<?> list = getHibernateTemplate().find("from " + Event.class.getSimpleName() + " order by date");
+        return (List<Event>) list;
+    }
+
 }
