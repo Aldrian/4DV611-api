d86225764f55acfe5b62508245a89fbad538a6eb
zayats <sanyazayats@gmail.com>
2015-12-15 12:36:13 +0100

    add filtering support to EventDao; order by date now it DESC

--- src/main/java/se/travappar/api/dal/impl/EventDAO.java
+++ src/main/java/se/travappar/api/dal/impl/EventDAO.java
@@ -3,6 +3,7 @@ package se.travappar.api.dal.impl;
 import org.springframework.transaction.annotation.Transactional;
 import se.travappar.api.dal.AbstractDAO;
 import se.travappar.api.model.Event;
+import se.travappar.api.model.filter.Filtering;
 
 import java.util.List;
 
@@ -14,8 +15,22 @@ public class EventDAO extends AbstractDAO<Event> {
 
     @Transactional
     @Override
-    public List<Event> getList() {
-        List<?> list = getHibernateTemplate().find("from " + Event.class.getSimpleName() + " order by date");
+    public List<Event> getList(List<Filtering> filteringList) {
+        StringBuilder stringBuilder = new StringBuilder("");
+        if(filteringList != null && !filteringList.isEmpty()) {
+            stringBuilder.append(" WHERE ");
+            for (Filtering filtering : filteringList) {
+                String relation = "";
+                if (filtering.getRelationWithPrevious() != null) {
+                    relation = filtering.getRelationWithPrevious();
+                }
+                stringBuilder.append(" ").append(relation).append(" ")
+                        .append(filtering.getColumnName()).append(" ")
+                        .append(filtering.getOperator()).append(" ")
+                        .append(filtering.getValue()).append(" ");
+            }
+        }
+        List<?> list = getHibernateTemplate().find("FROM " + Event.class.getSimpleName() + stringBuilder.toString() + " ORDER BY date DESC");
         return (List<Event>) list;
     }
 
